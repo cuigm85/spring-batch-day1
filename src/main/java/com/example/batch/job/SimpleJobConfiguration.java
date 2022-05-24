9b9d18d55f6bad3167e8827a4ca0165e455f68c4
Guangmin Cui <cuiguangmin@gmail.com>
2022-05-24 12:19:23 +0900

    part 3-2: BATCH_JOB_EXECUTION(2)

--- src/main/java/com/example/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/batch/job/SimpleJobConfiguration.java
@@ -32,7 +32,9 @@ public class SimpleJobConfiguration {
     public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
-                    throw new IllegalArgumentException("step1에서 실패합니다.");
+                    log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
+                    return RepeatStatus.FINISHED;
                 })
                 .build();
     }
